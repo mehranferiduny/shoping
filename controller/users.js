64c26c30c5f1c36aed92bc321da9272462251dd2
mehran feriduni <m3hr4nus@gmail.com>
2024-01-01 15:28:36 +0330

    add edit page user

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
 const Users = require('../models/Users');
+const Category=require('../models/Category')
+const Products=require('../models/Products');
 
 //!Users
 exports.addUsers = async (req, res) => {
@@ -51,6 +53,56 @@ exports.addUsers = async (req, res) => {
   }
 };
 
+exports.editUser=async(req,res)=>{
+    
+  const errorArr = [];
+  const UserID = req.params.id;
+  
+  
+
+  if(!UserID) res.status(404).send('ProductsID requrd')
+
+
+
+
+  try {
+     
+    const user = await Users.findOne({ _id: UserID });
+    const category=await Category.find({}).sort({ name: -1 });
+
+      if (!user) {
+          return res.redirect("/404");
+      }
+
+     await Users.userValidationedit(req.body);
+      
+         
+
+          const { title, titlefa, description,number,berand, price} = req.body;
+       
+
+        
+          return res.status(200).redirect('/dashbord/getProducts');
+      
+  } catch (err) {
+      console.log(err);
+      err.inner.forEach((e) => {
+          errorArr.push({
+              name: e.path,
+              message: e.message,
+          });
+      });
+      res.render('admin/editproduct',{
+        pageTitle:'ویرایش کردن محصول',
+        layout:'./layouts/dashLayot',
+        path:'/getProducts',
+        category,
+        product,
+        errors:errorArr,
+      })
+  }
+}
+
 exports.loginUser = async (req, res, next) => {
 
   if (!req.body["g-recaptcha-response"]) {
@@ -85,11 +137,11 @@ exports.loginUser = async (req, res, next) => {
 exports.rememberMe = (req, res) => {
   
   if (req.body.remember == 'on') {
-  
     req.session.cookie.originalMaxAge = 24 * 60 * 60 * 1000; // 1 day 24
   } else {
     req.session.cookie.expire = null;
   }
+  console.log(req.session)
   
 
   res.redirect('/')
@@ -136,14 +188,18 @@ exports.loginPage = async (req, res) => {
 exports.editPage = async (req, res) => {
 
   try {
-
+    const category=await Category.find({}).sort({ name: -1 })
+    const products=await Products.find({}).sort({ createdAt: -1 })
+    console.log(req.user);
     res.render("index/editPage", {
       pageTitle: ' ویرایش کاربر ',
       path: "/",
       layout: './layouts/loginLayout',
       message: req.flash('message'),
       error: req.flash("error"),
-
+      category,
+      products,
+      user:req.user,
     })
 
   } catch (err) {
